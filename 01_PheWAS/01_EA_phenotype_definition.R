53bd3fd3f97c8d18f79a5e73dd200c5016b9c7f9
Chiara Auwerx <cauwerx@gmail.com>
2024-07-01 22:39:37 +0200

    Update 01_EA_phenotype_definition.R

--- 01_PheWAS/01_EA_phenotype_definition.R
+++ 01_PheWAS/01_EA_phenotype_definition.R
@@ -10,7 +10,7 @@ library(dplyr)
 # STEP 1: Load data
 ########################################################
 
-# Load eductaion-related phenotype data. besides the identifier (eid) column, the file has two columns:
+# Load education-related phenotype data. Besides the identifier (eid) column, the file has two columns:
 # age_end_edu (field #845): Negative values set to missing; Average over instances;
 # qualifications (field # 6138): Negative values set to missing; All values that are not 1 (= College or University degree) are set to 0; Maximum over instances retained 
 pheno <- as.data.frame(fread("education_phenotypes.txt"))
